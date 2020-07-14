1a4ee638c25cd8df6265ed7fe951bf7abd77a98c
stradivarus <dupa.madzia@gmail.com>
2020-07-14 16:01:35 +0100

    rearranged

--- application.py
+++ application.py
@@ -14,27 +14,40 @@ messages = {}
 
 @app.route("/")
 def index():
-    return render_template("index.html", channels = channel_list)
+    return render_template("index.html")
 
 @socketio.on("add channel")
 def new_channel(data):
     channel_name = data['channel-name']
-    channel_list.append(channel_name)
+    exists = 1
 
-    emit("announce channel", {'channelName' : channel_name}, broadcast=True)
+    if channel_name not in channel_list:
+        channel_list.append(channel_name)
+        exists = 0
+
+    emit("announce channel", {'channelName' : channel_name, 'exists' : exists, 'user' : data['user']}, broadcast=True)
+
 
 @socketio.on("new message")
 def new_message(data):
-    message = {'message' : data['message'], 'name' : data['name'], 'date' : data['date']}
+    channel = data['channel']
+    message = {'message' : data['message'], 'name' : data['name'], 'date' : data['date'], 'channel' : channel}
     
-    if not data['channel'] in messages:
-        messages[data['channel']] = []
-    messages[data['channel']].append(message)
-
-    emit("announce messages", messages, broadcast=True)
+    if not channel in messages:
+        messages[channel] = []
+    messages[channel].append(message)
+    
+    emit("announce message", message, broadcast=True)
 
 
 # APIS
 @app.route("/api/channels")
 def send_channels():
     return jsonify(channel_list)
+
+@app.route("/api/messages/<channel>")
+def send_messages(channel):
+    if not channel in messages:
+        return jsonify("empty")
+    else:
+        return  jsonify(messages[channel])
--- static/script.js
+++ static/script.js
@@ -1,94 +1,213 @@
+var noMessages = true;
+
 document.addEventListener('DOMContentLoaded', () => {
 
+    // user submitted the name form nad it's remembered in localStorage
+    if (document.querySelector('.user form')) {
+        document.querySelector('.user form').onsubmit = () => {
+            localStorage.setItem('name', document.querySelector('#name').value);
+            load();
+            return false;
+        };
+    };
+
+    if (localStorage.getItem('name')) {
+        load();
+    };
+
+
+    document.querySelector('#message').onkeyup = function(e) {
+        if (e.keyCode === 13 && !e.shiftKey) {
+            document.querySelector('.type-message button').click();
+        };
+    };
+    
+});
+
+
+function load() {
+    document.querySelector('.user').innerHTML = `<p>singed in as</p>
+    <strong>${localStorage.getItem('name')}</strong>`;
+    document.querySelector('.add-channel button').disabled = false;
 
     // get list of current channels from the server with AJAX
     const request = new XMLHttpRequest();
-    request.open('GET', '/api/channels')
 
-    request.onload = () => {
-        const channelList = JSON.parse(request.responseText);
-        var currentChannel;
+    request.onreadystatechange = function () {
 
-        // if user picked a name before, we display that name
-        if (localStorage.getItem('name')) {
-            changeSidebar(channelList);
+        if (this.readyState == 1) {
+            document.querySelector('.channel-list').innerHTML = "";
+        }
+
+        if (this.readyState == 4 && this.status == 200) {
+            const channelList = JSON.parse(request.responseText);
+
+            loadChannels(channelList);
+            loadMessages(false);
+
+            // we did all we had to do with loading channels
+
+            // integrating socketIO
+            var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+            socket.on('connect', () => {
+                document.querySelector(".add-channel form").onsubmit = () => {
+                    const channelName = document.querySelector('#channel-name').value;
+
+                    socket.emit('add channel', {'channel-name': channelName, 'user' : localStorage.getItem('name')});
+                    document.querySelector('#channel-name').value = '';
+                    return false;
+                };
+
+                document.querySelector(".type-message form").onsubmit = () => {
+                    const message = document.querySelector("#message").value;
+
+                    var today = new Date();
+                    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                    var dateTime = date + ' ' + time;
+
+                    socket.emit('new message', { 'channel': localStorage.getItem('channel'), 'message': message, 'name': localStorage.getItem('name'), 'date': dateTime });
+                    document.querySelector('#message').value = '';
+                    document.querySelector('#message').focus();
+                    return false;
+                };
+            });
+
+            socket.on('announce channel', data => {
+                if (data.exists) {
+                    if (data.user == localStorage.getItem('name')) {
+                        document.querySelectorAll('.channel-list li').forEach(li => {
+                            if (li.innerHTML === data.channelName) {
+                                li.click();
+                            };
+                        });
+                    };
+                } 
+                else {
+                    const li = document.createElement('li')
+                    li.innerHTML = data.channelName;
+                    document.querySelector('.channel-list ul').append(li);
+                    
+                    li.onclick = function () {
+                        localStorage.setItem('channel', li.innerHTML);
+                        loadMessages();
+                    };
+                }; 
+            });
+
+            socket.on('announce message', data => {
+                if (data.channel === localStorage.getItem('channel')) {
+                    createMessage(data);
+                    noMessages = false;
+                };
+            });
+
+            // user clicked on a chat name link
+            document.querySelectorAll('.channel-list li').forEach(item => {
+
+                item.onclick = function () {
+                    localStorage.setItem('channel', item.innerHTML);
+                    loadMessages();
+                };
+            });
+            
         };
-
-        // user submitted the name form nad it's remembered in localStorage
-        if (document.querySelector('.user form')) {
-            document.querySelector('.user form').onsubmit = () => {
-                const name = document.querySelector('#name').value;
-
-                if (!localStorage.getItem('name'))
-                    localStorage.setItem('name', name);
-                changeSidebar(channelList);
-
-                return false;
-            };
-        };
+    };
+    request.open('GET', '/api/channels');
+    request.send();
+}
 
 
-        // integrating socketIO
-        var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+function loadMessages(setActive=true) {
 
-        socket.on('connect', () => {
-            document.querySelector(".add-channel form").onsubmit = () => {
-                const channelName = document.querySelector('#channel-name').value;
+    if (localStorage.getItem('channel')) {
 
-                socket.emit('add channel', { 'channel-name': channelName });
-                document.querySelector('#channel-name').value = '';
-                return false;
-            };
+        document.querySelector('.type-message button').disabled = false;
+        document.querySelector('.type-message textarea').focus(); //this isnt working ?????
+        document.querySelector('.messages').innerHTML = '';
 
-            document.querySelector(".type-message form").onsubmit = () => {
-                const message = document.querySelector("#message").value;
-
-                var today = new Date();
-                var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-                var dateTime = date + ' ' + time;
-
-                socket.emit('new message', {'channel' : currentChannel, 'message': message, 'name': localStorage.getItem('name'), 'date': dateTime });
-                document.querySelector('#message').value = '';
-                return false;
-            }
-        });
-
-        socket.on('announce channel', data => {
-            const li = document.createElement('li')
-            li.innerHTML = data.channelName;
-            document.querySelector('.channel-list ul').append(li);
-        });
-
-        socket.on('announce messages', data => {
-            console.log(data);
-
-            const div = document.createElement('div') // add clas message to it and fill
-            //PUT MESSAGES FROM CURRENT CHANNEL IN DIV
-        });
-        
-        // user clicked on a chat name link
-        document.querySelectorAll('.channel-list li').forEach(item => {
-            item.onclick = function () {
-                currentChannel = this.innerHTML;
-                console.log(currentChannel)
-            };
-        });
+        if (setActive) {
+            document.querySelectorAll('.channel-list li').forEach(li => li.classList.remove("active"));
+            event.target.classList.add("active");
+        };   
     };
 
-    request.send();
-});
+    //get all messages for that channel below
+    const request = new XMLHttpRequest;
 
-function changeSidebar(channels) {
-    document.querySelector('.user').innerHTML = `<p>singed in as</p>
-        <strong>${localStorage.getItem('name')}</strong>`;
+    request.onload = function () {
+        const messages = JSON.parse(request.responseText);
+
+        if (messages == "empty") {
+            if (localStorage.getItem('channel')) {
+                document.querySelector('.messages').innerHTML = "<div class=\"alert-container\"><div class=\"alert-warning\">No messages yet!</div></div>";
+            }
+            noMessages = true;
+        }
+        else {
+            noMessages = false;
+            messages.forEach(message => createMessage(message));
+        };
+    };
+
+    request.open('GET', `/api/messages/${localStorage.getItem('channel')}`)
+    request.send();
+};
+
+
+function loadChannels(channels) {
 
     var result = '<ul>';
 
-    channels.forEach(channel =>
-        result += `<li>${channel}</li>`
-    );
-    result += '</ul>'
+    channels.forEach(channel => {
+        if (channel === localStorage.getItem('channel')) {
+            result += `<li class="active">${channel}</li>`;
+        }
+        else {
+            result += `<li>${channel}</li>`;
+        };
+    });
+    
+    result += '</ul>';
 
     document.querySelector('.channel-list').innerHTML = result;
 };
+
+// when we add a message we should check if there were any beforehand
+function createMessage(message) {
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="message">
+                            <div class="message-header">
+                                <strong>${message.name}</strong>
+                                <small>${message.date}</small></div>
+                                    <div class="message-text">
+                                        <p>${message.message}</p>
+                                    </div>
+                            </div >`
+
+    if (noMessages) {
+        document.querySelector('.messages').innerHTML = "";
+    };
+
+    messages = document.querySelector('.messages');
+    messages.append(div);
+    messages.scrollTop = messages.scrollHeight;
+};
+
+
+// Helpers - NOT SURE I NEED THIS ONE
+// function spacesToDashes(s) {
+//     var converted = "";
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === " ") {
+//             converted += "-";
+//         }
+//         else
+//         {
+//             converted += s[i];
+//         };
+//     };
+
+//     return converted;
+// };
